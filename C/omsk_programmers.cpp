cde48fef3606c64323bebbb8d363740db6593b52
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-06-14 21:54:46 +0100

    final solution

--- C/omsk_programmers.cpp
+++ C/omsk_programmers.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <climits>
 using namespace std;
 
-long long puissmax(long long x, long long a, long long b) {
-    long long p = 0;
-    while (!(a/((long long)pow(x,p)) >= b && a/((long long)pow(x,p+1)) < b)) {
-        p++;}
-    return p;}
+long long pm(long long x, long long a) {
+    long long i = 0;
+    while ((long long)pow(x, i) <= a) i++;
+    return i - 1;
+}
 
 int main() {
     int t;
@@ -18,9 +19,19 @@ int main() {
         long long m = min(a, b);
         a = max(a, b);
         b = m;
-        if (a == b) cout << 0 << "\n";
-        else if (a < x) cout << min(a-b, 2LL) << "\n";
-        else {
-            long long k = puissmax(x, a, b);
-            cout << min(k + (a/((long long)pow(x,k)) - b),
-                       k + 1 + (b - a/((long long)pow(x,k+1)))) << "\n";}}}
+        if (a == b) { cout << 0 << "\n"; continue; }
+        if (a < x) { cout << min(a-b, 2LL) << "\n"; continue; }
+        long long p = pm(x, a);
+        long long q = pm(x, b);
+        long long mi = LLONG_MAX;
+        for (long long i = 0; i <= p+1; i++) {
+            for (long long j = 0; j <= q+1; j++) {
+                long long u = a / (long long)pow(x, i);
+                long long v = b / (long long)pow(x, j);
+                long long d = (u >= v) ? (u - v) : (v - u);
+                mi = min(mi, i + j + d);
+            }
+        }
+        cout << mi << "\n";
+    }
+}
